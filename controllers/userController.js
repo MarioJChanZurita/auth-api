592410dd3a9beb9e50247349cdc11a7501af1642
MarioJChanZurita <mariojesuschanzurita@hotmail.com>
2022-02-08 00:06:59 -0600

    Update

---
+++ controllers/userController.js
@@ -0,0 +1,2 @@
+const mysqlCon = require('../db/mysqlCon')
+
---
+++ db/mysqlCon.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql')
+
+const LOCAL_CONFIG = {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "AppsManager"
+}
+
+const SERVER_CONFIG = {
+
+}
+
+const mysqlCon = mysql.createConnection(LOCAL_CONFIG || SERVER_CONFIG)
+
+module.exports = mysqlCon
+
+
+
+
+
+
+/* 
+
+USERS DATA
+-name 
+-email
+-role -> permissions 
+-apps allowed 
+
+
+APPS DATA
+
+
+
+*/
